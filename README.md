43e53358f68890c5fa196ae73e8f5baea00326fe
Zach Estreito <zachestreito@gmail.com>
2022-02-27 01:29:39 -0800

    Updated readme and added create canvas assignment function

--- README.md
+++ README.md
@@ -2,9 +2,3 @@ jc_library is a library for interfacing between Jupyter and Canvas, with support
 
 ## Usage
 * Work in progress
-
-
-## Example command line usage:
-```
-python3 -c "import jc_library; jc_library.print_students('~/testcourse/')"
-```
--- jc_library.py
+++ jc_library.py
@@ -1,18 +1,10 @@
-import sys, os
+import sys, os, configparser
 from nbgrader.api import Gradebook
 from nbgrader.plugins.export import ExportPlugin
 from canvasapi import Canvas
 
-# Canvas API URL
-API_URL = "https://canvas.instructure.com/"
-# Canvas API key
-API_KEY = "7~rXjqr1vzqXOsu1hV9a7nZSlwOoaVbXUPluL9EyjrxOt6w4wyR8Q7SO9SMt9Wr37U"
-# Canvas Course ID (found in the course URL)
-COURSE_ID = 4374071
 
-# Initialize a new Canvas object
-canvas = Canvas(API_URL, API_KEY)
-
+########## INITIALIZATION FUNCTIONS ##########
 
 # sets current gradebook.db for internal use
 def __set_db(course_dir):
@@ -32,9 +24,24 @@ def __set_course(course_id):
 		sys.exit(e) # prints error and exits program
 
 
+########## PRIVATE NBGRADER FUNCTIONS ##########
+
+# DEBUGGING FUNCTION to print nbgrader db student list
+def __nb_print_students(course_dir):
+	for student in gradebook.students:
+		print(student)
+	gradebook.close()
+
+
+# DEBUGGING FUNCTION to print nbgrader db assignment list
+def __nb_print_assignments(course_dir):
+	for assignment in gradebook.assignments:
+		print(assignment)
+	gradebook.close()
+
+
 # INCOMPLETE returns a 2d array in the format of assignment_array[string studentID, float grade]
 def __nb_get_assignment_grades(course_dir, assignment):
-	gradebook = __set_db(course_dir)
 	exporter = ExportPlugin(gradebook)
 	try:
 		print("")
@@ -47,7 +54,6 @@ def __nb_get_assignment_grades(course_dir, assignment):
 # adds students to local nbgrader db after checking if student already exists or not
 # expects arg 2 to be an array of string NetIDs
 def __nb_add_students(course_dir, students):
-	gradebook = __set_db(course_dir)
 	for student in students:
 		try:
 			gradebook.add_student(student)
@@ -59,7 +65,6 @@ def __nb_add_students(course_dir, students):
 # removes student from local nbgrader db
 # expects arg 2 to be student ID as string
 def __nb_remove_student(course_dir, student):
-	gradebook = __set_db(course_dir)
 	try:
 		gradebook.remove_student(student)
 	except Exception as e:
@@ -67,38 +72,42 @@ def __nb_remove_student(course_dir, student):
 	gradebook.close()
 
 
-# DEBUGGING FUNCTION to print nbgrader db student list
-def __nb_print_students(course_dir):
-	gradebook = __set_db(course_dir)
-	for student in gradebook.students:
-		print(student)
-	gradebook.close()
-
+########## PRIVATE CANVAS FUNCTIONS ##########
 
 # DEBUGGING FUNCTION to print canvas course student list
 def __c_print_students(course_id):
-	course = __set_course(course_id)
 	print("\n---%s Student List---" % course)
-	for i in course.get_users(enrollment_type = "student"):
+	for i in course.get_users(enrollment_type = ['student']):
 		print(i)
-
-
-# DEBUGGING FUNCTION to print nbgrader db assignment list
-def __nb_print_assignments(course_dir):
-	gradebook = __set_db(course_dir)
-	for assignment in gradebook.assignments:
-		print(assignment)
-	gradebook.close()
 
 
 # DEBUGGING FUNCTION to print canvas course student list
 def __c_print_assignments(course_id):
-	course = __set_course(course_id)
 	print("\n---%s Assignment List---" % course)
 	for i in course.get_assignments():
 		print(i)
 
+# Create new assignment within canvas
+def __c_create_assignment(assignment_name):
+	course.create_assignment({
+		'name': assignment_name
+	})
+
+
+############################################################################
+
+# Initialize config
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Initialize Canvas objects
+canvas = Canvas(config['Canvas']['API_URL'], config['Canvas']['API_KEY'])
+course = __set_course(int(config['Canvas']['COURSE_ID']))
+
+# Initialize nbgrader database
+gradebook = __set_db(config['nbgrader']['COURSE_DIRECTORY'])
 
 ### TESTING ZONE
-__c_print_students(COURSE_ID)
-__c_print_assignments(COURSE_ID)
+__c_create_assignment('ps1')
+__c_print_students(course)
+__c_print_assignments(course)
