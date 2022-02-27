a424712fae5e897dba9c6f417fbeba63b4e58a74
Zach Estreito <zachestreito@gmail.com>
2022-02-27 00:11:45 -0800

    Started integrating Canvas API

--- jc_library.py
+++ jc_library.py
@@ -1,30 +1,77 @@
 import sys, os
 from nbgrader.api import Gradebook
+from nbgrader.plugins.export import ExportPlugin
+from canvasapi import Canvas
+
+# Canvas API URL
+API_URL = ""
+# Canvas API key
+API_KEY = ""
+# Canvas Course ID (found in the course URL)
+COURSE_ID = 123
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
 
 
-# adds students to db after checking if student already exists or not
-# expects arg 2 to be an array of strings
-def add_students(course_dir, students):
+# INCOMPLETE returns a 2d array in the format of assignment_array[string studentID, float grade]
+def get_assignment_grades(course_dir, assignment):
+	gradebook = __set_db(course_dir)
+	exporter = ExportPlugin(gradebook)
+	try:
+		print("")
+	except Exception as e:
+		print(e)
+	gradebook.close()
+	return # finish this
+
+
+# adds students to local nbgrader db after checking if student already exists or not
+# expects arg 2 to be an array of string NetIDs
+def nb_add_students(course_dir, students):
 	gradebook = __set_db(course_dir)
 	for student in students:
-		gradebook.update_or_create_student(student)
+		try:
+			gradebook.add_student(student)
+		except Exception as e:
+			print("%s (%s)" % (e, student))
 	gradebook.close()
 
-# print list of students
-def print_students(course_dir):
+
+# removes student from local nbgrader db
+# expects arg 2 to be student ID as string
+def nb_remove_student(course_dir, student):
+	gradebook = __set_db(course_dir)
+	try:
+		gradebook.remove_student(student)
+	except Exception as e:
+		print(e)
+	gradebook.close()
+
+
+# DEBUGGING FUNCTION to print nbgrader db student list
+def nb_print_students(course_dir):
 	gradebook = __set_db(course_dir)
 	for student in gradebook.students:
 		print(student)
 	gradebook.close()
 
-# print list of assignments
-def print_assignments(course_dir):
+
+# DEBUGGING FUNCTION to print canvas course student list
+def c_print_students(course_id):
+	course = __set_course(course_id)
+	for i in course.get_users():
+		print(i)
+
+# DEBUGGING FUNCTION to print nbgrader db assignment list
+def nb_print_assignments(course_dir):
 	gradebook = __set_db(course_dir)
 	for assignment in gradebook.assignments:
 		print(assignment)
 	gradebook.close()
 
-# set gradebook.db for internal use
+
+# sets current gradebook.db for internal use
 def __set_db(course_dir):
 	try:
 		course_dir = os.path.expanduser(course_dir) # this line accommodates for ~/ usage
@@ -32,3 +79,15 @@ def __set_db(course_dir):
 		return Gradebook("sqlite:///gradebook.db")
 	except Exception as e:
 		sys.exit(e) # prints error and exits program
+
+
+# sets the active Canvas course for API use
+def __set_course(course_id):
+	try:
+		return canvas.get_course(course_id)
+	except Exception as e:
+		sys.exit(e) # prints error and exits program
+
+
+### TESTING ZONE
+c_print_students(COURSE_ID)
